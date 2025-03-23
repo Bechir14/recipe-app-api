a284ea9df6a16071bf69a18faa2e9dcea3262d5a
bechir <mebnou14@gmail.com>
2025-03-23 21:19:12 +0300

    added wait for db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,9 @@
 #django command to wait for the db
 
+import time 
 
+from psycopg2 import OperationalError as psycopg2OpError
+from django.db.utils import OperationalError
 
 
 from django.core.management.base import BaseCommand
@@ -9,4 +12,19 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        pass
+        """entry point for the command """
+
+        self.stdout.write('waiting for database ')
+        db_up = False
+
+        while db_up is False :
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (psycopg2OpError , OperationalError):
+                self.stdout.write('Database unavailable waiting 2 seconds....')
+                time.sleep(2) 
+
+        self.stdout.write(self.style.SUCCESS("Database available"))
+
+        
--- app/core/tests/tests_command.py
+++ app/core/tests/tests_command.py
@@ -4,7 +4,7 @@
 
 from unittest.mock import patch
 
-from Psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError 
 from django.test import SimpleTestCase 
@@ -20,7 +20,7 @@ class CommandTest(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_ckeck.assert_called_once_with(database=['default'])
+        patched_ckeck.assert_called_once_with(databases=['default'])
     @patch('time.sleep')
     def test_wait_for_db_delay(self ,patched_sleep, patched_check ):
         patched_check.side_effect = [Psycopg2Error] * 2 + \
@@ -29,9 +29,4 @@ class CommandTest(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count , 6)
-        patched_check.assert_called_with(database=['default'])
-
-
-
-        
-        
+        patched_check.assert_called_with(databases=['default'])
