82cfdb78413d8c41e67f0e49166b118b7c45695b
bechir <mebnou14@gmail.com>
2025-03-23 22:41:24 +0300

    fixed linting errors in wait for sb command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,30 +1,26 @@
-# django command to wait for the db
+# Django command to wait for the database
 
-import time 
-
+import time
 from psycopg2 import OperationalError as psycopg2OpError
 from django.db.utils import OperationalError
-
-
 from django.core.management.base import BaseCommand
 
 
-
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        """entry point for the command """
+    """Django command to wait for the database to be available."""
 
-        self.stdout.write('waiting for database ')
+    def handle(self, *args, **options):
+        """Entry point for the command."""
+
+        self.stdout.write("Waiting for database...")
         db_up = False
 
-        while db_up is False :
+        while not db_up:
             try:
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 db_up = True
-            except (psycopg2OpError , OperationalError):
-                self.stdout.write('Database unavailable waiting 2 seconds....')
-                time.sleep(2) 
+            except (psycopg2OpError, OperationalError):
+                self.stdout.write("Database unavailable, waiting 2 seconds...")
+                time.sleep(2)
 
-        self.stdout.write(self.style.SUCCESS("Database available"))
-
-        
+        self.stdout.write(self.style.SUCCESS("Database available!"))
